8b0d30770b7df68fb9aa1b2781e4615d34eed37e
Unknown <ranjan.poudel@selu.edu>
2019-03-12 07:20:53 -0500

    Test data insert statements

---
+++ Database/InitialTestData/Organizations_201903120719.sql
@@ -0,0 +1,3 @@
+﻿INSERT INTO public."Organizations" ("Id","Name","AddedDate","UpdatedDate","IsActive") VALUES 
+('36dbc88c-79b1-4810-a688-88ef9e1d7d0b','Southeastern Louisiana University','2019-01-31 00:00:00.000','2019-01-31 00:00:00.000','2019-01-31 00:00:00.000')
+;
