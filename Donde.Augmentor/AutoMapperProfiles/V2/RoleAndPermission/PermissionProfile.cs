707f8d3627b7e817c6967e1e190694603b30ef6c
Ranjan Poudel <ranjanpoudel1234@gmail.com>
2021-01-28 17:41:17 -0600

    fixed permission endpoint

--- Donde.Augmentor/AutoMapperProfiles/V2/RoleAndPermission/PermissionProfile.cs
+++ Donde.Augmentor/AutoMapperProfiles/V2/RoleAndPermission/PermissionProfile.cs
@@ -13,6 +13,9 @@ namespace Donde.Augmentor.Web.AutoMapperProfiles.V2.RoleAndPermission
               .ForMember(x => x.Resource, opts => opts.MapFrom(src => src.Permission.Resource))
               .ForMember(x => x.Description, opts => opts.MapFrom(src => src.Permission.Description))
               .ForMember(x => x.Action, opts => opts.MapFrom(src => src.Permission.ResourceAction));
+
+            CreateMap<Permission, PermissionViewModel>()
+              .ForMember(x => x.Action, opts => opts.MapFrom(src => src.ResourceAction));
         }
     }
 }
